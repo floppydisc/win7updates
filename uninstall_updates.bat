492da2b44b89719bae8a443091fe9fe60f367a21
David Kikl <d.kikl@gmx.net>
2015-10-10 11:21:53 +0200

    resolves error in updatelisting & improves updatedeinstallation (win7 german only)

--- uninstall_updates.bat
+++ uninstall_updates.bat
@@ -5,15 +5,16 @@ rem hier die nur die KB - NUMMERN also ohne "KB" eingeben:
 set HIDE_KBS=3035583
 
 set INSTALLED_UPDATES=%cd%\installed_updates.txt
-set UNINSTALLED_UPDATES=%cd%\uninstalled_updates.txt
-set UPDATE_INFO=%TMP%\tmp_update_info.txt
+set UNINSTALL_UPDATES=%cd%\uninstall_updates_found.txt
+set UPDATE_INFO=%cd%\tmp_update_info.txt
 
 set findkb="false"
 set uninstalled="false"
 
 echo (1/3)Erstelle Liste installierter Updates nach
 echo %installed_updates%
-start /WAIT /B dism /online /get-packages /format:table > %installed_updates%
+del %installed_updates%
+dism /online /get-packages /format:table > %installed_updates%
 echo\
 echo -----------------------------
 echo (2/3)Suche und deinstalliere Updates (%UNINST_KBS%)
@@ -22,21 +23,31 @@ for %%K in (%UNINST_KBS%) do (
 	echo Suche %%K
 	set findkb=false
 
-	echo %INSTALLED_UPDATES% | findstr /B /R /C:"\<Package_for_%%kb[^\|]*" >%UPDATE_INFO%
-	set /p pkg_info=<%UPDATE_INFO%
+	type %INSTALLED_UPDATES% | findstr /B /R /C:"\<Package_for_%%K.*Installiert[^\|]*" >%UPDATE_INFO%
+	
+	rem echo liste installierter updates...	
+	rem type %UPDATE_INFO%
+	rem PAUSE
+	for /f "tokens=*" %%r in (%UPDATE_INFO%) do (
 
-	for /f "tokens=1 delims= " %%G IN ("%pkg_info%") DO (
-		set findkb=true
-		echo %%K gefunden - wird deinstalliert
-		echo %pkg_info% >>%UNINSTALLED_UPDATES%
-		start /WAIT /B dism /online /Remove-Package /PackageName:%%G /quiet /norestart
-	)
+		rem set /p pkg_info=<"%%r"
+		rem echo R = %%r
 
-	if %findkb%=="true" ( 
-		echo\
-	) ELSE (
-		echo Update %%K nicht gefunden.
-		echo\
+		for /f "tokens=1 delims= " %%G IN (%%r) DO (
+		echo deinstalliere %%r..
+		PAUSE
+			set findkb=true
+			echo %%G gefunden - wird deinstalliert
+			echo %%r >>%UNINSTALL_UPDATES%
+			start /WAIT /B dism /online /Remove-Package /PackageName:%%G /quiet /norestart
+		)
+
+		if %findkb%=="true" ( 
+			echo\
+		) ELSE (
+			echo Update %%K nicht am System gefunden.
+			echo\
+		)
 	)
 )
 echo\
